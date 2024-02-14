5e074e3f2ea53c796538d50682ee8a338f8bdc84
andrewbatallones <batallonesa@gmail.com>
2024-02-14 10:39:50 -0500

    Add tailwind and flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev" class="underline underline-offset-1">kit.svelte.dev</a> to read the documentation</p>
